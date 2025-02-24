00d5c8176505b3c1df2ecd1db0f91b6a82274d1c
Neqaative <87881872+Neqaative@users.noreply.github.com>
2025-02-24 17:38:45 +0100

    add files

---
+++ PASiR_Kozuch_Filip/src/main/java/com/example/pasir_kozuch_filip/PaSiRKozuchFilipApplication.java
@@ -0,0 +1,16 @@
+package com.example.pasir_kozuch_filip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaSiRKozuchFilipApplication {
+
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaSiRKozuchFilipApplication.class, args);
+	}
+
+
+}
---
+++ PASiR_Kozuch_Filip/src/test/java/com/example/pasir_kozuch_filip/PaSiRKozuchFilipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pasir_kozuch_filip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaSiRKozuchFilipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
